c21d28ca752e60a099a2c3a433171a7358ad3091
obermoser <bernhard.obermoser@icloud.com>
2025-05-16 18:56:51 +0200

    Working

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-amber-100">
-      <h1 className="text-3xl text-brand font-bold">StoreIt</h1>
+      <h1 className="text-3xl font-poppins font-bold">StoreIt</h1>
     </div>
   );
 }
