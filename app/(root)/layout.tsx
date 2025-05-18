769ee187e693ea69394a3f64d10fd0c8d0cd94e7
obermoser <bernhard.obermoser@icloud.com>
2025-05-18 22:08:48 +0200

    Root Layout start

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="flex h-screen">
+      <Sidebar/>
+      <section className="h-full flex-1 flex-col">
+        MobileNavigation Header
+        <div className="main-content">{children}</div>
+      </section>
+    </main>
+  );
+};
+
+export default Layout;
