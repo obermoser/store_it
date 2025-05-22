b57bd5ace4784caab959af223debd3b732adfe98
Bernhard <bernhard.obermoser@icloud.com>
2025-05-22 13:36:10 +0200

    Uploader Start

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+"use client";
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
+import { cn, getFileType } from "@/lib/utils";
+import Image from "next/image";
 
-const FileUploader = () => {
-  return (
-    <div>FileUploader</div>
-  )
+interface Props {
+  ownerId: string;
+  accountId: string;
+  className?: string;
 }
 
-export default FileUploader
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} className="cursor-pointer" />
+      <Button type="button" className={cn("uploader-button", className)}>
+        <Image
+          src="/assets/icons/upload.svg"
+          alt="Upload"
+          width={24}
+          height={24}
+        />
+        <p>Upload</p>
+      </Button>
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uploading</h4>
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name);
+          })}
+        </ul>
+      )}
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag &apos;n drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
